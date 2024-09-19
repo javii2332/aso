f00f904e2bc53aeb8e3d7550b33f7842a995eebc
javii2332 <javierlluesma23@gmail.com>
2024-09-19 12:51:25 +0200

    NUEVA CARPETA PARA UNIDAD 1

---
+++ ejercicio14.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Comprobamos que se han pasado los argumentos correctos
+if [ "$#" -lt 3 ] && [ "$#" -gt 4 ]; then
+    echo "Error. La sintaxis correcta es ./gestionusuarios.sh alta/baja nombre apellido1 apellido2 [grupo]"
+    exit 1
+fi
+
+accion=$1
+nombre=$2
+apellido1=$3
+apellido2=$4
+grupo=${5:-""} # Si no se pasa grupo, se deja vacío
+
+# Función para generar el identificador
+generar_identificador() {
+local id="alu${apellido1:0:2}${apellido2:0:2}${nombre:0:1}"
+echo "$id"
+}
+
+# Acción de alta
+if [ "$accion" == "alta" ]; then
+id_usuario=$(generar_identificador)
+
+# Si no se indica grupo, se crea uno nuevo
+if [ -z "$grupo" ]; then
+grupo=$id_usuario
+groupadd "$grupo"
+fi
+
+# Se crea el usuario
+useradd -m -g "$grupo" "$id_usuario"
+echo "Usuario $id_usuario creado y agregado al grupo $grupo."
+
+# Acción de baja
+elif [ "$accion" == "baja" ]; then
+id_usuario=$(generar_identificador)
+
+# Se elimina el usuario
+userdel -r "$id_usuario"
+echo "Usuario $id_usuario eliminado."
+
+else
+echo "Error. La sintaxis correcta es ./gestionusuarios.sh alta/baja nombre apellido1 apellido2 [grupo]"
+exit 1
+
