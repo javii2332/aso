61adb140765e6a4e44fce9fb00059a1a8675599a
javii2332 <javierlluesma23@gmail.com>
2024-12-19 11:29:52 +0100

    NUEVOS EJERCICIOS

---
+++ U6_PWS_EJERCICIOS/EJERCICIO2_P2.ps1
@@ -0,0 +1,14 @@
+﻿[int]$num1 = Read-Host "Escribe un numero porfavor"
+[int]$num2 = Read-Host "Escribe otro numero porfavor"
+
+$suma= $num1 + $num2
+$resta= $num1 - $num2
+$multiplicacion= $num1 * $num2
+$division= $num1 / $num2
+$resto= $num1 % $num2
+
+Write-Host "RESULTADO DE LA SUMA =" $suma
+Write-Host "RESULTADO DE LA RESTA =" $resta 
+Write-Host "RESULTADO DE LA MULTIPLICACION =" $multiplicacion 
+Write-Host "RESULTADO DE LA DIVISION =" $division
+Write-Host "RESULTADO DEL RESTO =" $resto 
---
+++ U6_PWS_EJERCICIOS/EJERCICIO3_P2.ps1
@@ -0,0 +1,5 @@
+﻿[double]$num_h = Read-Host "Escribe el numero de horas trabajadas"
+[double]$cost_h = Read-Host "Escribe el coste de una hora tuya de trabajo"
+$total = $num_h * $cost_h 
+
+Write-Host "TOTAL A COBRAR" $total
