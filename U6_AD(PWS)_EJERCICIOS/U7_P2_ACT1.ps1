5c0e0873987615d1264228c3cc0aa8defae087d3
javii2332 <javierlluesma23@gmail.com>
2025-01-20 13:51:58 +0100

    NUEVO SCRIPT

---
+++ U6_AD(PWS)_EJERCICIOS/U7_P2_ACT1.ps1
@@ -0,0 +1,17 @@
+﻿#ACTIVIDAD 1
+
+# 1 VISUALIZAR CARPETAS
+Write-Host "VISUALIZANDO CARPETAS COMPARTIDAS (EXCLUIDAS ESPECIALES)"
+Get-SmbShare | Where-Object { $_.Name -notin 'NETLOGON', 'SYSVOL' } |  Where-Object { $_.Name -notlike '*$' }
+
+# 2 CREAR CARPETA ASO
+Write-Host "CREANDO NUEVA CARPETA..."
+ New-Item -Path C:\ASO -ItemType Directory
+
+ # 3 COMPARTIR LA CARPETA ASO 
+ Write-Host "COMPARTIENDO LA CARPETA ASO"
+ New-SmbShare -Path C:\ASO -Name aso
+
+ # 4 
+
+ # 5 PERMISOS DE ASO 
