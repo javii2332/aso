83cfa1aeff5e1c46d55e5e92520e2da94f36c7e8
javii2332 <javierlluesma23@gmail.com>
2025-01-22 08:28:34 +0100

    ej1

--- U6_AD(PWS)_EJERCICIOS/U7_P2_ACT1.ps1
+++ U6_AD(PWS)_EJERCICIOS/U7_P2_ACT1.ps1
@@ -13,5 +13,12 @@ Write-Host "CREANDO NUEVA CARPETA..."
  New-SmbShare -Path C:\ASO -Name aso
 
  # 4 
+ #Al compartir la carpeta aso, los permisos por defecto asignados son:
+ #-Grupo "Todos" (Everyone): Se otorgan permisos de lectura para todos los usuarios. Esto permite que cualquier usuario pueda acceder a la carpeta en modo de solo lectura, pero no realizar modificaciones.
+ #-Administrador local: Los usuarios con privilegios administrativos en el sistema tienen acceso total a la carpeta, lo que incluye lectura, escritura y control total.
+ #-Sistema: El sistema operativo tiene acceso total para gestionar operaciones necesarias en la carpeta.
 
  # 5 PERMISOS DE ASO 
+ Revoke-SmbShareAccess -Name ASO -AccountName Todos -Force
+ Grant-SmbShareAccess -Name ASO -AccountName profesores -AccessRight Full -Force
+ Grant-SmbShareAccess -Name ASO -AccountName alumnos -AccessRight Read -Force  
